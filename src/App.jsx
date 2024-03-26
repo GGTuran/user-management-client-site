3e94b691a806dea3393d8a1ff508cf1c532e8994
GGTuran <tayyabturan67@gmail.com>
2024-03-26 23:32:08 +0600

    form added and data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,15 @@ function App() {
     .then(res=> res.json())
     .then(data => setUsers(data));
   } ,[])
+
+  const formSubmit = event =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email};
+    console.log(user);
+  }
   
 
   return (
@@ -19,6 +28,17 @@ function App() {
      
       <h1>User Management</h1>
       <h1>Numbers:{users.length}</h1>
+      <form onSubmit={formSubmit} >
+        <input type="text" name='name' id='' />
+        <br />
+        <input type="email" name='email' id='' />
+        <input type="submit" value="Add User" />
+      </form>
+      <div>
+        {
+          users.map(user=> <p key={user.id}>{user.id} : {user.name} </p>)
+        }
+      </div>
 
     </>
   )
